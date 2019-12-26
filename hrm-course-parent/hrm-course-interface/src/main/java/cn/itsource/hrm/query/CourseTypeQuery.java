ed218a4ca2d64f6e921efa4af80d21fb50d63913
liyang <liyang@itsource>
2019-12-26 10:37:24 +0800

    v1.2

---
+++ hrm-course-parent/hrm-course-interface/src/main/java/cn/itsource/hrm/query/CourseTypeQuery.java
@@ -0,0 +1,18 @@
+package cn.itsource.hrm.query;
+
+
+
+import cn.itsource.util.query.BaseQuery;
+
+
+/**
+ * <p>
+ * 课程目录 查询参数对象
+ * </p>
+ *
+ * @author LY
+ * @since 2019-12-26
+ */
+public class CourseTypeQuery extends BaseQuery {
+
+}
